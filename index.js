065ec4f4d4fccc7816e04eeb64b3b8dd255ad019
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-04 13:19:57 +0530

    express installed and localhost is running fine

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Kese ho, thk ho!")
+const express = require('express')
+const app = express()
+
+app.get('/',(req,res) => {
+	res.send("kese ho, thk ho!")
+})
+
+app.listen(5000, () => {
+	console.log("port is running on paanch hazar!")
+})
