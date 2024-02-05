d1788fa96111eeb0989cdbcb39a511be6079b327
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-05 22:48:12 +0530

    made some  routes and installed dotenv for production

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,6 +6,17 @@ app.get('/',(req,res) => {
 	res.send("kese ho, thk ho!")
 })
 
-app.listen(5000, () => {
+app.get('/aditya',(req,res) =>{
+	res.send("<h2> Konse class me pdhte ho</h2>")
+})
+
+app.get('/aayein',(req,res) => {
+	res.send("Favourite subject kia h tumhara???")
+})
+
+app.get('/baingan',(req,res) => {
+	res.send("<h1> Kia naam h tumhara </h1>")
+})
+app.listen(process.env.PORT, () => {
 	console.log("port is running on paanch hazar!")
 })
