dd35de421d14c008cc3ffbae245d08f7abd5dd28
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-10 11:16:27 +0530

    build the react app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.static('dist'))
 
 app.get('/',(req,res) => {
 	res.send("kese ho, thk ho!")
