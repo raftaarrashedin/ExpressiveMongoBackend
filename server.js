58f0d7615377f82b3286bd91695dca57b46f00b5
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-10 10:10:55 +0530

    early updation on server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+	res.send('Server is Ready!')
+})
+
+app.get('/jokes',(req,res) => {
+	const jokes = 
+	[
+	    {
+	        "id": 1,
+	        "title": "Sparkling Eyes",
+	        "flirty_lines": "Your eyes are like stars in the night sky, sparkling and mesmerizing."
+	    },
+	    {
+	        "id": 2,
+	        "title": "Smile Magic",
+	        "flirty_lines": "Your smile could light up the darkest of rooms, it's truly magical."
+	    },
+	    {
+	        "id": 3,
+	        "title": "Charm Unleashed",
+	        "flirty_lines": "Do you have a map? I keep getting lost in the charm of your eyes."
+	    },
+	    {
+	        "id": 4,
+	        "title": "Heartbeat Symphony",
+	        "flirty_lines": "Every time I see you, my heart beats in a rhythm only you can hear."
+	    },
+	    {
+	        "id": 5,
+	        "title": "Moonlit Beauty",
+	        "flirty_lines": "You're as beautiful as the moonlit night, casting enchantment wherever you go."
+	    }
+	]
+
+})
+
+app.listen(port, () => {
+	console.log(' Serve at http://localhost:${port}');
+})
