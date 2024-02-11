6974a18e34910a67bea2f09c120c6cc3ed8d63bc
raftaar rashedin <raftaarrashedin100@gmail.com>
2024-02-11 11:23:29 +0530

    created data modeling with mongoose

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose"
 
-const subTodo_schema = new mongoose.Schema({},{timestamps : true})
+const subTodo_schema = new mongoose.Schema({
+
+	content : {
+		type : String,
+		required : true
+	},
+	complete : {
+		type : Boolean,
+		default : false
+	},
+	createdBy : {
+		type : mongoose.Schema.Types.ObjectId,
+		ref : "User"
+	}
+
+},{timestamps : true})
 
 export const SubTodo = mongoose.model("SubTodo", subTodo_schema)
